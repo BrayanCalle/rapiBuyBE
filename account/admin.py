76f47dde49e907137bdb8869b5d85ca1b0e0c74d
Bryan Calle <bryan08_2011@hotmail.com>
2021-07-20 23:48:00 -0500

    Adding dependencies in pylint.

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,15 @@
+"""
+admin
+"""
 from django.contrib import admin
 
-# Register your models here.
+
+class CategoriaAdmin(admin.ModelAdmin):
+    """
+    Administracion de categoria
+    """
+
+    list_display = ("name", "description")
+    search_fields = ("name", "description")
+    list_filter = ("name", "description")
+    fieldsets = ((("Información de Categoría"), {"fields": ("name", "description")}),)
